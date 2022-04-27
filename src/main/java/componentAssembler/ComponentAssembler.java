f4656688322810dfc7a41787e064a18e8396cb02
LenaS1098 <lena_schollmayer@web.de>
2022-04-27 14:47:28 +0200

    added todos ComponentAssembler

---
+++ src/main/java/componentAssembler/ComponentAssembler.java
@@ -0,0 +1,36 @@
+package componentAssembler;
+
+public class ComponentAssembler {
+
+
+    public void startLU(){
+        // TODO: 27.04.2022 starten der Laufzeitumgebung; int als return zur Kontrolle
+    }
+
+    public void stopLU(){
+        // TODO: 27.04.2022 stoppen der Laufzeitumgebung; int als return zur Kontrolle
+    }
+
+    public void addComponent(String jarPath){
+        // TODO: 27.04.2022 add einer Komponente aus lokalem Verzeichnis
+    }
+
+    public void startComponent(int componentId){
+        // TODO: 27.04.2022 Starten einer bestimmten Komponente; anhand ID
+    }
+
+    public void stopComponent(int componentId){
+        // TODO: 27.04.2022 Stoppen einer bestimmten Komponente; anhand ID
+    }
+
+    public void deleteComponent(int componentId){
+        // TODO: 27.04.2022 Entfernen einer bestimmten Komponente; anhand ID
+    }
+
+    public void getStatus(){
+        // TODO: 27.04.2022 Status aller Komponenten wird ausgegeben
+    }
+    public void getStatusById(int componentId){
+        // TODO: 27.04.2022 Status einer Komponente (byId) wird ausgegeben
+    }
+}
